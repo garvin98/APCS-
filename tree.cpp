1b029f15dcdf573485cb5ddfab931913184ea9df
garvin98 <s1130180@dmhs.kh.edu.tw>
2026-04-21 10:41:22 +0800

    Create tree.cpp
    Signed-off-by: garvin98 <s1130180@dmhs.kh.edu.tw>

---
+++ tree.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout <<"--正直角三角形--\n";
+	for(int i=1; 1<=5;i++){
+		for(int j=1; j<= i;j++) {
+			cout <<"*";
+	}
+	cout<< "\n";
+	}
+	return 0;
+}
